7f3f41d49840130407cd3df0b847e1aacd78b6fa
AndrewCrossan1 <andrew.crossan23@outlook.com>
2022-01-18 22:51:35 +0000

    Email class tested thoroughly, results proved positive

---
+++ VerificationTest/EmailVerification.cs
@@ -0,0 +1,57 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Threading.Tasks;
+using Verification;
+
+namespace VerificationTest
+{
+    [TestClass]
+    public class EmailVerification
+    {
+        [Description("A syntactically valid, domain valid email"), TestCategory("Email Testing"), TestMethod]
+        public async Task ValidEmail() 
+        {
+            Email email = new Email("Andrew.crossan23@outlook.com");
+            bool valid = await email.VerifyAsync();
+            if (!valid)
+            {
+                Console.WriteLine(Email.ErrorMessage);
+            }
+            else
+            {
+                Console.WriteLine("Valid email");
+                Assert.AreEqual(valid, true);
+            }
+        }
+        [Description("A syntactically valid, domain invalid email"), TestCategory("Email Testing"), TestMethod]
+        public async Task InvalidDomain()
+        {
+            Email email = new Email("Andrew.crossan23@tigtogdfsadf.com");
+            bool valid = await email.VerifyAsync();
+            if (!valid)
+            {
+                Console.WriteLine(Email.ErrorMessage);
+            }
+            else
+            {
+                Console.WriteLine("Invalid email");
+                Assert.AreEqual(valid, false);
+            }
+        }
+        [Description("A syntactically invalid, domain valid email"), TestCategory("Email Testing"), TestMethod]
+        public async Task InvalidSyntax()
+        {
+            Email email = new Email("1!||@:;'w.#rossan23@outlook.com");
+            bool valid = await email.VerifyAsync();
+            if (!valid)
+            {
+                Console.WriteLine(Email.ErrorMessage);
+            }
+            else
+            {
+                Console.WriteLine("Invalid email");
+                Assert.AreEqual(valid, false);
+            }
+        }
+    }
+}
