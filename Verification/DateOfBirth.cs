9d17722a07ae8e01c0271631d5536de4fb531a98
AndrewCrossan1 <andrew.crossan23@outlook.com>
2022-01-23 23:07:50 +0000

    Date of birth verification added, validates if user is over 18,  and if the date is in the future or not. Testing proves positive.

---
+++ Verification/DateOfBirth.cs
@@ -0,0 +1,87 @@
+﻿namespace Verification
+{
+    /// <summary>
+    /// Class for verifying Date of Births, returns true or false.
+    /// </summary>
+    public class DateOfBirth
+    {
+        #region Variable Declaration
+        //Public property for value attribute
+        public DateTime Value { get { return value; } set { this.value = value; } }
+        //Private attribute for value
+        private DateTime value = default;
+        //Private Variable for ErrorMessage
+        /// <summary>
+        /// Saves error message if present
+        /// </summary>
+        private static string? errormessage;
+        //Public property for ErrorMessage attribute
+        public static string? ErrorMessage => errormessage;
+        #endregion
+
+        public DateOfBirth(DateTime date)
+        {
+            value = date;
+        }
+
+        #region Method Declaration
+        /// <summary>
+        /// Check if inputted date is valid (Is date in the future?)
+        /// </summary>
+        /// <returns>True or false</returns>
+        public bool ValidateDate()
+        {
+            try
+            {
+                if (DateOnly.FromDateTime(this.value) > DateOnly.FromDateTime(DateTime.Today))
+                {
+                    errormessage = "Date must be not in the future!";
+                    return false;
+                }
+                else
+                {
+                    return true;
+                }
+            }
+            catch (Exception ex)
+            {
+                errormessage = "An internal error has occurred";
+                return false;
+            }
+        }
+        /// <summary>
+        /// Check if user is older than or equal to 18
+        /// </summary>
+        /// <returns>True or false</returns>
+        public bool IsUserOldEnough() 
+        {
+            //Check if year difference is 18
+            if (DateOnly.FromDateTime(DateTime.Today).Year - DateOnly.FromDateTime(this.value).Year < 18)
+            {
+                errormessage = "User is not old enough.";
+                return false;
+            } 
+            else if (DateOnly.FromDateTime(DateTime.Today).Year - DateOnly.FromDateTime(this.value).Year > 18) 
+            {
+                return true; 
+            }
+            else
+            { 
+                if (value.DayOfYear < DateTime.Today.DayOfYear) 
+                {
+                    errormessage = "User is not old enough.";
+                    return false;
+                } 
+                else if (value.DayOfYear > DateTime.Today.DayOfYear) 
+                {
+                    return false;
+                }
+                else 
+                {
+                    return true;
+                }
+            }
+        }
+        #endregion
+    }
+}
---
+++ VerificationTest/DOBTest.cs
@@ -0,0 +1,63 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using Verification;
+namespace VerificationTest
+{
+    [TestClass]
+    public class DOBTest
+    {
+        [Description("Testing a valid 18 year old DOB"), TestCategory("Date of Birth Testing"), TestMethod()]
+        public void Over18() 
+        {
+            DateOfBirth dob = new DateOfBirth(Convert.ToDateTime("23-01-2004"));
+            bool result2 = dob.ValidateDate();
+            bool result1 = dob.IsUserOldEnough();
+            if (result1 == false || result2 == false)
+            {
+                Console.WriteLine(DateOfBirth.ErrorMessage);
+                Assert.AreEqual(false, result1);
+                Assert.AreEqual(false, result2);
+            }
+            else
+            {
+                Assert.AreEqual(true, result1);
+                Assert.AreEqual(true, result2);
+            }
+        }
+        [Description("Testing an under 18 year olds DOB"), TestCategory("Date of Birth Testing"), TestMethod()]
+        public void Under18()
+        {
+            DateOfBirth dob = new DateOfBirth(Convert.ToDateTime("19-10-2005"));
+            bool result1 = dob.IsUserOldEnough();
+            bool result2 = dob.ValidateDate();
+            if (result1 == false || result2 == false)
+            {
+                Console.WriteLine(DateOfBirth.ErrorMessage);
+                Assert.AreEqual(false, result1);
+            }
+            else
+            {
+                Assert.AreEqual(true, result1);
+                Assert.AreEqual(true, result2);
+            }
+        }
+        [Description("Testing an under 18 year olds DOB"), TestCategory("Date of Birth Testing"), TestMethod()]
+        public void InFuture()
+        {
+            DateOfBirth dob = new DateOfBirth(Convert.ToDateTime("19-10-2025"));
+            bool result1 = dob.IsUserOldEnough();
+            bool result2 = dob.ValidateDate();
+            if (result1 == false || result2 == false)
+            {
+                Console.WriteLine(DateOfBirth.ErrorMessage);
+                Assert.AreEqual(false, result1);
+                Assert.AreEqual(false, result2);
+            }
+            else
+            {
+                Assert.AreEqual(true, result1);
+                Assert.AreEqual(true, result2);
+            }
+        }
+    }
+}
