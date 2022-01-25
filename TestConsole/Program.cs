012a28b072a124753fcde38e19ab58f9342a7120
AndrewCrossan1 <andrew.crossan23@outlook.com>
2022-01-25 21:55:11 +0000

    New project called TestConsole created, used for testing synchronous methods within package.

---
+++ TestConsole/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Verification;
+
+namespace TestConsole
+{
+    public class Program 
+    {
+        static void Main(string[] args) 
+        {
+            bool valid = Email.SendConfirmation("andrew.crossan23@gmail.com", "Bruichladdich23!", "andrew.crossan23@gmail.com");
+            Console.Write(valid);
+        }
+    }
+}
