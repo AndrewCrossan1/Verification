06b5f9818212c1e464c4d316fd86a0bb200f2018
AndrewCrossan1 <andrew.crossan23@outlook.com>
2022-01-25 22:02:53 +0000

    Username class added

---
+++ Verification/Username.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Verification
+{
+    /// <summary>
+    /// Used to verify usernames, validate user names against an array of other usernames to ensure unique and exclusivity.
+    /// </summary>
+    public class Username
+    {
+        #region Variable Declaration
+        //Public property for value attribute
+        public string Value { get { return value; } set { this.value = value; } }
+        //Private attribute for value
+        private string value = default;
+        //Private Variable for ErrorMessage
+        /// <summary>
+        /// Saves error message if present
+        /// </summary>
+        private static string? errormessage;
+        //Public property for ErrorMessage attribute
+        public static string? ErrorMessage => errormessage;
+        #endregion
+
+        //Constructor Method
+        public Username(string Username)
+        {
+            this.value = Username;
+        }
+
+        #region Method Declaration
+
+        #endregion
+    }
+}
